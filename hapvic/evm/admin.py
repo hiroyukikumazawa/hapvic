dbf2801cad48e74be97a4890b4c26572f73ba0c5
Hiroyuki <hiroyukikumazawa.jp@gmail.com>
2023-11-23 13:24:56 +0100

    add admin.py in evm app

--- hapvic/evm/admin.py
+++ hapvic/evm/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Chain, Contract, RPCUrl
+
+
+@admin.register(Chain)
+class ChainAdmin(admin.ModelAdmin):
+    list_display = ("chain_id", "chain_symbol", "chain_name", "chain_description")
+    search_fields = ("chain_symbol", "chain_name")
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ("chain_id", "contract_address", "contract_name", "active", "creation_date", "creator")
+    list_filter = ("chain_id", "active")
+    search_fields = ("chain_id__chain_symbol", "contract_address", "contract_name", "creator")
+    readonly_fields = ("creation_date",)  # If you want to make creation_date read-only
+
+
+@admin.register(RPCUrl)
+class RPCUrlAdmin(admin.ModelAdmin):
+    list_display = ("chain_id", "url", "active")
+    list_filter = ("chain_id", "active")
+    search_fields = ("chain_id__chain_symbol", "url")
