c512fe0a3af9bc27b6303d8fb765480212610132
Hiroyuki <hiroyukikumazawa.jp@gmail.com>
2023-11-30 03:20:51 +0900

    Add api router and update user model

--- config/api_router.py
+++ config/api_router.py
@@ -1,15 +1,7 @@
-from django.conf import settings
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from django.urls import include, path
 
-from hapvic.users.api.views import UserViewSet
-
-if settings.DEBUG:
-    router = DefaultRouter()
-else:
-    router = SimpleRouter()
-
-router.register("users", UserViewSet)
-
+urlpatterns = [
+    path("users/", include("hapvic.users.api.urls")),
+]
 
 app_name = "api"
-urlpatterns = router.urls
